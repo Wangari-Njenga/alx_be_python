b63ce8e800b4faa1ad02fd219db69900d25e30ac
Wangari-Njenga <njengawangari20@gmail.com>
2025-09-16 18:31:55 +0300

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+
+principal = 1000  # $1000
+rate = 0.05       # 5% annual interest
+time = 3          # 3 years
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
